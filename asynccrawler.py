ce997da34e3ac003e1c95feec25da1b9327ab95f
TimtheChemist <timothy.kwok.e@gmail.com>
2025-12-27 21:30:18 +0000

    Add async crawl_page()

--- asynccrawler.py
+++ asynccrawler.py
@@ -23,7 +23,7 @@ class AsyncCrawler():
             if normalized_url in self.page_data:
                 return False
             else: 
-                self.page_data[normalized_url] = True
+                self.page_data[normalized_url] = {}
                 return True
 
 
@@ -45,3 +45,48 @@ class AsyncCrawler():
         except Exception as e:
             print(f"Something went wrong! {e}")
             return None
+
+
+
+
+    async def crawl_page(self, current_url=None):
+        if current_url == None:
+            current_url = self.base_url
+
+
+        normalised_current_url = normalize_url(current_url)
+
+        current_url_object = urlparse(current_url)
+        base_url_object = urlparse(self.base_url)
+
+
+        if current_url_object.netloc != base_url_object.netloc:
+            return self.page_data
+
+        if await self.add_page_visit(normalised_current_url) == False:
+            return self.page_data
+
+        async with self.semaphore:
+            html = await self.get_html(current_url)
+        
+        if html is None:
+            return self.page_data
+        
+        async with self.lock:
+            self.page_data[normalised_current_url] = extract_page_data(html, current_url)
+        
+        print(f"Getting page data from {current_url}")
+
+        url_list = get_urls_from_html(html, current_url)
+
+
+
+        tasks = []
+        for url in url_list:   
+            task = asyncio.create_task(self.crawl_page(url))
+            tasks.append(task)
+
+        await asyncio.gather(*tasks)
+
+
+        return self.page_data
